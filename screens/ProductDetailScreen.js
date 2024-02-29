46177146b499f3c8a7440aad3d12c7ca7390b6a4
Kavindu Thiwanka <101232856+kavindu1thiwanka@users.noreply.github.com>
2024-02-29 09:46:04 +0530

    update

--- screens/ProductDetailScreen.js
+++ screens/ProductDetailScreen.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { View, Text, Image, StyleSheet } from 'react-native';
+import AddToCartButton from '../components/AddToCartButton';
 
 const ProductDetailScreen = ({ route }) => {
   // Extract the product details from the route params
   const { shoe } = route.params;
+
+  const addToCart = () => {
+    alert('Product added to cart');
+  };
 
   return (
     <View style={styles.container}>
@@ -11,8 +16,9 @@ const ProductDetailScreen = ({ route }) => {
       <View style={styles.detailsContainer}>
         <Text style={styles.name}>{shoe.name}</Text>
         <Text style={styles.brand}>{shoe.brand}</Text>
-        <Text style={styles.price}>{shoe.price}</Text>
+        <Text style={styles.price}>LKR. {shoe.price}</Text>
         <Text style={styles.description}>{shoe.description}</Text>
+        <AddToCartButton onPress={addToCart} />
       </View>
     </View>
   );
